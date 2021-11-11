e9492417c036e5dbe0a07b5e475ea5212c278b39
Talex210 <tal.89@mail.ru>
2021-11-11 21:17:04 +0300

    Add files

---
+++ Virtual Piano/task/src/index.js
@@ -0,0 +1,27 @@
+document.addEventListener('keydown', function(event){
+    if (event.code === 'KeyA') {
+        new Audio('audio/A.mp3').play().then();
+    } else if (event.code === 'KeyS') {
+        new Audio('audio/S.mp3').play().then();
+    } else if (event.code === 'KeyD') {
+        new Audio('audio/D.mp3').play().then();
+    } else if (event.code === 'KeyF') {
+        new Audio('audio/F.mp3').play().then();
+    } else if (event.code === 'KeyG') {
+        new Audio('audio/G.mp3').play().then();
+    } else if (event.code === 'KeyH') {
+        new Audio('audio/H.mp3').play().then();
+    } else if (event.code === 'KeyJ') {
+        new Audio('audio/J.mp3').play().then();
+    } else if (event.code === 'KeyW') {
+        new Audio('audio/W.mp3').play().then();
+    } else if (event.code === 'KeyE') {
+        new Audio('audio/E.mp3').play().then();
+    } else if (event.code === 'KeyT') {
+        new Audio('audio/T.mp3').play().then();
+    } else if (event.code === 'KeyY') {
+        new Audio('audio/Y.mp3').play().then();
+    } else if (event.code === 'KeyU') {
+        new Audio('audio/U.mp3').play().then();
+    }
+});
